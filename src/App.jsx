3e3e342601d5b39c47aa35a2eee795412f9d7fc4
Yunus İnoluk <yunusinoluk@gmail.com>
2022-03-20 19:43:39 +0300

    Footer added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import ProductDetail from "./pages/ProductDetail";
 
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/products/:id" element={<ProductDetail />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div style={{ backgroundColor: "#f8f9fa" }}>
+      <div className="container d-flex justify-content-end py-3">
+        <span style={{ fontSize: "14px" }}>Copyright 2022 - Yunus İnoluk</span>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
